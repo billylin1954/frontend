d1c604b859cf2485cba093ebf95937c82716d5f1
Billy <billyLin1954@gmail.com>
2024-12-12 21:33:30 -0500

    f

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
-
+function Videos(props){
+  //let films=['./videos/0001-0250.mp4','./videos/0001-0030.mp4']
+  
+  return(
+    <video width="250" height="250" controls>
+      <source src="/videos/0001-0250.mp4" type="video/mp4"/>
+      <source src="/videos/0001-0250.webm" type="video/webm"/>
+    </video>
+  );
+}
 function App() {
+  
   return (
     <div>
       <h1>yubtub</h1>
       <br></br>
       <h1>random videos</h1>  
+      <Videos/>
     </div>
 
   );
